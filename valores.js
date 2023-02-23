dcf8df310d594242db14e1b4118d31f606ab4555
César Gabriel Rodríguez Acosta <cesardz_2000_00@outlook.com>
2023-02-22 22:04:07 -0600

    Initial Exercises Added

---
+++ valores.js
@@ -0,0 +1,11 @@
+40
+"Cesar Acosta"
+
+true
+false
+
+null
+undefined
+
+[1, 2, 3]
+{ nombre: "Cesar"}
---
+++ variables.js
@@ -0,0 +1,31 @@
+var nombre = "Cesar";
+
+//Declarar
+var edad;
+//Inicializar
+edad = 30;
+edad
+
+//String
+var elementos = ["Computadora", "Celular"];
+
+//Objeto
+var persona = {
+    nombre: "Diego",
+    edad: 30
+}
+
+persona;
+
+
+
+
+
+
+
+
+
+
+
+
+
